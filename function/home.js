4b7722fb9ba69165602120adf00f2b4a9581efb4
Riyannnnn <riyanshaikh19gmail.com>
2022-03-24 19:34:50 +0530

    rating form

--- function/home.js
+++ function/home.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("sidenav").style.height = "100%";
   }
   
   function closeNav() {
     document.getElementById("sidenav").style.width = "0";
+    document.getElementById("sidenav").style.height = "0";
   }
