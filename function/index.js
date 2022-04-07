1368e5c8c7c7627324e776955681e4e398ff44a8
Riyannnnn <riyanshaikh19gmail.com>
2022-04-07 11:34:00 +0530

    delete

--- function/index.js
+++
@@ -1,6 +0,0 @@
-function redirectLogin (){
-    window.location.href = "http://127.0.0.1:5501/view/login.html";
-}
-function redirectRegister(){
-    window.location.href = "http://127.0.0.1:5501/view/register.html";
-}
